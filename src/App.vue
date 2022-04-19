4d2d08a881da8747e227a3fd0d33faaff0d227a8
fengchuan <1299693999@qq.com>
2022-04-20 00:48:23 +0800

    微信pchook

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="display: flex;">
+
+
+  <div class="left" style="display: flex;flex-direction: column">
+    <div>
+<el-button @click="lunchwechat">启动微信</el-button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(item,index) of friends" :key="index" @click="selectedwxid=item.wxid">{{item.wxid}}</li>
+      </ul>
+    </div>
+  </div>
+
+    <div class="right">
+      <div>
+      <div>
+        <el-input v-model="selectedwxid"></el-input>
+      </div>
+
+      <div>
+        <el-input v-model="content"></el-input>
+      </div>
+        <div>
+          <el-button @click="sendmsg">发送</el-button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script setup>
+import {ref, onMounted} from "vue";
+import axios from "axios"
+const friends=ref([])
+const selectedwxid=ref("")
+onMounted(()=>{
+ axios.request({method:'get',url:'/api/msg/getfriendslist/'}).then(ret=>{
+  console.log('1111111',ret.data.data)
+   friends.value=ret.data.data
+ })
+})
+const lunchwechat=()=>{
+  window.lunchwechat()
+}
+const content=ref("")
+const sendmsg=()=>{
+  // eslint-disable-next-line no-empty
+  if (content.value && selectedwxid.value){
+    window.sendmsg(selectedwxid.value,content.value)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+
+    host: "192.168.0.245",
+    open: true,
+    hot: true,//自动保存
+    proxy: {
+      '/api/': {
+        target: 'http://192.168.0.245:8000',
+        changeOrigin: true
+
+      }
+    }
+  },
 })
